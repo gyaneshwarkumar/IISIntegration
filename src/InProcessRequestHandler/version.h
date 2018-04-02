7b32246d767d24a7092a63225f63e984f574ad55
Justin Kotalik <jukotali@microsoft.com>
2018-04-02 15:09:45 -0700

    version

--- src/InProcessRequestHandler/version.h
+++ src/InProcessRequestHandler/version.h
@@ -5,9 +5,9 @@
 // This file is auto-generated
 
 
-#define FileVersion 8,1,18091,0
-#define FileVersionStr "8.1.18091.0\0"
-#define ProductVersion 8,1,18091,0
-#define ProductVersionStr "8.1.18091.0\0"
+#define FileVersion 8,1,18092,0
+#define FileVersionStr "8.1.18092.0\0"
+#define ProductVersion 8,1,18092,0
+#define ProductVersionStr "8.1.18092.0\0"
 #define PlatformToolset "v141\0"
-#define CommitHash "\0"
+#define CommitHash "f203ce471c32ad78a54d6a64f8d0b70332fdde13\0"
